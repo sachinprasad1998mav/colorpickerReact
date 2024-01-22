595c623992b4df48550bd78441ceeb33289d5a4d
Sachin Prasad <91291081+sachinprasad1998mav@users.noreply.github.com>
2024-01-22 12:43:14 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import ColorPicker from "./colorPicker";
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+/>;
+
+function App() {
+  return <ColorPicker></ColorPicker>;
+}
+
+export default App;
